e83f307d1552699f05e81f5ff1cdda86b661f657
CarlWork <carllworks@gmail.com>
2023-03-27 16:35:07 -0500

    FinishedProgressFeature

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,50 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    deinit {
+        timer?.invalidate()
+    }
+    
+    var timer: Timer?
+    var remainingTime = 0
+    
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720
+    ]
+    
+    @IBAction func hardnesSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
 
-
+        if let result = eggTimes[hardness] {
+            remainingTime = result
+            progressBar.progress = 1.0
+            startTimer()
+        }
+    }
+    func startTimer() {
+        timer?.invalidate()
+        
+        let totalTime = remainingTime
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
+            guard let self = self else { return }
+            self.remainingTime -= 1
+            
+            let progress = Float(self.remainingTime) / Float(totalTime)
+            self.progressBar.setProgress(progress, animated: true)
+            print("Time remaining: \(self.remainingTime)")
+            
+            if self.remainingTime <= 0 {
+                self.timer?.invalidate()
+                print("Egg is ready")
+            }
+        }
+    }
+    
 }
